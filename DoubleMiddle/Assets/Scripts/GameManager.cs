d00ec63992df814c4e99a3dc793e91f60f7a9529
rlagusrn08 <rlagusrn08@gmail.com>
2019-09-16 23:31:06 +0900

    노트 구현

---
+++ DoubleMiddle/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //싱글 톤
+    public static GameManager instance { get; set; }
+
+
+    public void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+    }
+
+    //노트들의 스피드 정하기
+    public float noteSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DoubleMiddle/Assets/Scripts/NoteBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBehavior : MonoBehaviour
+{
+    public int noteType;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        //노트를 아래로 움직이게 함
+        transform.Translate(Vector3.down * GameManager.instance.noteSpeed);
+    }
+}
